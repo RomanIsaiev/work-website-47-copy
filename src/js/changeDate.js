9c0f458e58e8ad6eb557ae22b07b4ef930f27bc9
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-08-23 11:44:01 +0300

    Update changeDate.js

--- src/js/changeDate.js
+++ src/js/changeDate.js
@@ -1,30 +1,30 @@
 // change day
 
 var currentDate = new Date();
-currentDate.setDate(currentDate.getDate() + 1);
+currentDate.setDate(currentDate.getDate());
 var day = currentDate.getDate();
 var month = currentDate.getMonth() + 1;
 var year = currentDate.getFullYear();
 var formattedDate = day;
-document.getElementById('dateElement').innerHTML = formattedDate;
+document.getElementById("dateElement").innerHTML = formattedDate;
 
 // **************************************************************************
 
 var current_date = new Date();
 current_date.setDate(current_date.getDate() + 1);
 var month = new Array(
-  'ЯНВАРЯ',
-  'ФЕВРАЛЯ',
-  'МАРТА',
-  'АПРЕЛЯ',
-  'МАЯ',
-  'ИЮНЯ',
-  'ИЮЛЯ',
-  'АВГУСТА',
-  'СЕНТЯБРЯ',
-  'ОКТЯБРЯ',
-  'НОЯБРЯ',
-  'ДЕКАБРЯ'
+  "ЯНВАРЯ",
+  "ФЕВРАЛЯ",
+  "МАРТА",
+  "АПРЕЛЯ",
+  "МАЯ",
+  "ИЮНЯ",
+  "ИЮЛЯ",
+  "АВГУСТА",
+  "СЕНТЯБРЯ",
+  "ОКТЯБРЯ",
+  "НОЯБРЯ",
+  "ДЕКАБРЯ"
 );
 
 // var month = new Array(
@@ -41,7 +41,7 @@ var month = new Array(
 //   'ЛИСТОПАДА',
 //   'ГРУДНЯ'
 // );
-document.getElementById('monthElement').innerHTML =
+document.getElementById("monthElement").innerHTML =
   month[current_date.getMonth()];
 
 // var month = new Array(
